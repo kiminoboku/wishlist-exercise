3b530149354b504f2fe9a51db82d4f64093a953a
MaciekDrobot <49610218+MaciejDrobot@users.noreply.github.com>
2020-08-11 15:52:45 +0100

    Added app skeleton (#3)
    * Added app skeleton
    Co-authored-by: Radoslaw Lewandowski <kiminoboku@gmail.com>

---
+++ src/main/java/com/example/wishlistexercise/WishlistExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.wishlistexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishlistExerciseApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(WishlistExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wishlistexercise/WishlistExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wishlistexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
